ebbeb4880e7de4cff1de6ed33d335f0c43c2f2e0
msmetanin10 <makarsmetanin159@gmail.com>
2025-04-30 18:20:15 +0500

    v1.1
    added ball!!!

--- ping_pong.py
+++ ping_pong.py
@@ -32,10 +32,6 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
-
-class Ball(GameSprite):
-    def update(self):
-        pass
         
 win_width = 700
 win_height = 500
@@ -45,12 +41,18 @@ display.set_caption("ping pong game pravda 102%")
 background = transform.scale(image.load(img_back), (win_width,win_height))
 window.blit(background,(0,0))
 
+ball = GameSprite(img_ball,324,224,50,50,3)
+
 playerl = Player(img_player,30,win_height - 270,10,150,5)
 playerr = Player(img_player,win_width - 35,win_height - 270,10,150,5)
 
 font.init()
-font1 = font.SysFont('Arial',70)
-win_text = font1.render('YOU WIN!', True, (255,215,0))
+font1 = font.Font(None,35)
+lose_text_l = font1.render('PLAYER 1 LOSE!', True, (255,0,0))
+lose_text_r = font1.render('PLAYER 2 LOSE!', True, (255,0,0))
+
+speed_x = 3
+speed_y = 3
 
 game = True
 finish = False
@@ -64,6 +66,12 @@ while game:
         if e.type == KEYDOWN:
             if e.key == K_r and finish == True:
                 #PEREZAPUSK!!!
+                speed_x = 3
+                speed_y = 3
+                ball.rect.x = 324
+                ball.rect.y = 224
+                playerl.rect.y = win_height - 270
+                playerr.rect.y = win_height - 270
                 finish = False
 
     if finish != True:
@@ -72,8 +80,33 @@ while game:
         playerl.updatel()
         playerr.updater()
 
+        if ball.rect.y >= win_height-50 or ball.rect.y <= 0:
+            speed_y *= -1.1
+            if speed_y > 50:
+                speed_y = 50
+            if speed_y < -50:
+                speed_y = -50
+
+        if ball.rect.x <= 0:
+            finish = True
+            window.blit(lose_text_l,(200,200))
+        if ball.rect.x >= win_width-50:
+            finish = True
+            window.blit(lose_text_r,(200,200))
+
+        if sprite.collide_rect(ball,playerl) or sprite.collide_rect(ball,playerr):
+            speed_x *= -1.1
+            if speed_x > 50:
+                speed_x = 50
+            if speed_x < -50:
+                speed_x = -50
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
         playerl.reset()
         playerr.reset()
+        ball.reset()
 
         display.update()
     clock.tick(FPS)
